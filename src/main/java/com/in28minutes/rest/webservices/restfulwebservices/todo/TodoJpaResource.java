08d6de78135559798e2d3e7312a7003f77019777
srezania <s.rezaniahandelsblattgroup.com>
2024-07-23 10:25:52 +0200

    connencting to H2 database

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
@@ -1,43 +1,55 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
+import com.in28minutes.rest.webservices.restfulwebservices.todo.repository.TodoRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@RestController
-public class TodoResorce {
+@RestController
+public class TodoJpaResource {
     private TodoService todoService;
+    private TodoRepository todoRepository;
 
-    public TodoResorce(TodoService todoService) {
+    public TodoJpaResource(TodoService todoService, TodoRepository todoRepository) {
         this.todoService = todoService;
+        this.todoRepository = todoRepository;
     }
 
     @GetMapping(path="/users/{username}/todos")
     public List<Todo> retrieveTodos(@PathVariable String username){
-            return todoService.findByUsername(username);
+            //return todoService.findByUsername(username);
+        return todoRepository.findByUsername(username);
+
     }
 
     @GetMapping(path="/users/{username}/todos/{id}")
     public Todo retrieveTodo(@PathVariable String username, @PathVariable int id ){
-        return todoService.findById(id);
+        //return todoService.findById(id);
+        return todoRepository.findById(id).get();
     }
 
     @DeleteMapping(path="/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id ){
-         todoService.deleteById(id);
+         //todoService.deleteById(id);
+        todoRepository.deleteById(id);
          return ResponseEntity.noContent().build();//return a success. we can use public void ...
     }
     @PutMapping (path="/users/{username}/todos/{id}")
     public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo ){
-        todoService.updateTodo(todo);
+        //todoService.updateTodo(todo);
+        todoRepository.save(todo);
         return todo;
     }
 
+
     @PostMapping (path="/users/{username}/todos")
     public Todo createTodo(@PathVariable String username, @RequestBody Todo todo ){
-        Todo createdTodo = todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
-        return createdTodo;
+        todo.setUsername(username);
+        todo.setId(null);
+       return todoRepository.save(todo);
+        //Todo createdTodo = todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+       // return createdTodo;
     }
 
 }
---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into todo (id, description, done, target_date, username )
+values (10001, 'learn1', false, CURRENT_DATE(), 'sima');
+
+insert into todo (id, description, done, target_date, username )
+values (10002, 'learn2', false, CURRENT_DATE(), 'sima');
+
+insert into todo (id, description, done, target_date, username )
+values (10003, 'learn3', false, CURRENT_DATE(), 'sima');
